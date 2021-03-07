7b608adcea110235c7d001c8c8fc43cbcdd3b3ed
Shinbatsu <nojic132@gmail.com>
2021-03-07 10:40:34 +0300

    supported instruction list

--- src/Grammar.hs
+++ src/Grammar.hs
@@ -31,3 +31,46 @@ data ProgramLine = InstrLine Instruction
                  | LabelLine Lbl
                  | Empty
                  deriving (Eq, Show)
+
+data Instruction = Inc Reg
+                 | Dec Reg
+                 | Mov Reg Arg -- binary instrs
+                 | Add Reg Arg
+                 | Sub Reg Arg
+                 | Mul Arg
+                 | Div Reg Arg -- Int division
+                 | Xor Reg Arg 
+                 | And Reg Arg
+                 | Or  Reg Arg
+                 | Not Reg
+                 | Neg Reg
+                 | Shr Reg Arg
+                 | Shl Reg Arg
+                 | Rol Reg Arg
+                 | Ror Reg Arg
+                 | Cmp Arg Arg 
+                 | Jmp Lbl -- jumps
+                 | Jne Lbl
+                 | Je  Lbl
+                 | Jge Lbl
+                 | Jl  Lbl
+                 | Jg  Lbl
+                 | Jle Lbl
+                 | Jo  Lbl
+                 | Jc  Lbl
+                 | Jp  Lbl
+                 | Js  Lbl
+                 | Jno Lbl
+                 | Jnc Lbl
+                 | Jnp Lbl
+                 | Jns Lbl
+                 | Call Lbl
+                 | Push Arg
+                 | Pop Reg
+                 | PushF
+                 | PopF
+                 | Msg [Msg]
+                 | Ret
+                 | End
+                 | UnsafeEnd
+                 deriving (Eq, Show)
